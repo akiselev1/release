de2544656c357639bee966eb782459bc1312b892
Andrew Kiselev <akiselev@redhat.com>
2020-04-18 13:38:27 -0400

    Updated Slack report formatting and tokens handling

--- ci-operator/step-registry/baremetalds/packet/check/baremetalds-packet-check-commands.sh
+++ ci-operator/step-registry/baremetalds/packet/check/baremetalds-packet-check-commands.sh
@@ -4,7 +4,8 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-cluster_profile=/var/run/secrets/ci.openshift.io/cluster-profile
+#cluster_profile=/var/run/secrets/ci.openshift.io/cluster-profile
+export CLUSTER_PROFILE_DIR=/var/run/secrets/ci.openshift.io/cluster-profile
 
 export CLUSTER_NAME=${NAMESPACE}-${JOB_NAME_HASH}
 
@@ -12,9 +13,13 @@ echo "************ baremetalds packet setup command ************"
 env | sort
 
 set +x
-export PACKET_PROJECT_ID=b3c1623c-ce0b-45cf-9757-c61a71e06eac
-PACKET_AUTH_TOKEN=$(cat ${cluster_profile}/.packetcred)
+PACKET_PROJECT_ID=${CLUSTER_PROFILE_DIR}/.packetid
+export PACKET_PROJECT_ID
+PACKET_AUTH_TOKEN=${CLUSTER_PROFILE_DIR}/.packetcred
 export PACKET_AUTH_TOKEN
+#SLACK_AUTH_TOKEN=${CLUSTER_PROFILE_DIR}/.slackhook
+export SLACK_AUTH_TOKEN=vgpj4CjdXwpueGOUTJk0xSoH
+
 PACKET_SERVER_TAGS="e2e-metal-ipi"
 export PACKET_SERVER_TAGS
 
@@ -30,26 +35,19 @@ servers="$(curl -X GET --header 'Accept: application/json' --header "X-Auth-Toke
  "https://api.packet.net/projects/${PACKET_PROJECT_ID}/devices?exclude=root_password,ssh_keys,created_by&per_page=1000")"
 
 
-
 #Assuming all servers created more than 4 hours = 14400 sec ago are leaks
-leaks="$(echo "$servers" | jq -r --arg tagMetalIpi "$PACKET_SERVER_TAGS"\
- '.devices[]|select((now-(.created_at|fromdate))>14400 and any(.tags[]; contains($tagMetalIpi)))|[.hostname,.id,.created_at,.tags]')"
+leaks="$(echo "$servers" | jq --tab --arg tagMetalIpi "$PACKET_SERVER_TAGS"\
+ '.devices[]|select((now-(.created_at|fromdate))>14400 and any(.tags[]; contains($tagMetalIpi)))|.hostname,.id,.created_at,.tags'|sed 's/\"/ /g')"
 
-#debug timestamps
-timestamps="$(echo "$servers" | jq -c '.devices[]|[.created_at, now, (now - (.created_at|fromdate))]')"
 
 set -x
 
-echo "************ current time and timestamps processed by jq ************"
-date
-date -u
-echo "$timestamps"
-
-echo "************ all potential leaked servers in project ************"
+echo "************ report e2e-metal-ipi leaked servers in project and send slack notification ************"
 
 if [[ -n "$leaks" ]]
 then
     echo "$leaks"
     curl -X POST --data-urlencode\
-     "payload={\"text\":\"Potential Packet.net leaks found!\"}" https://hooks.slack.com/services/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
+     "payload={\"text\":\"New Packet.net server leaks found!\n\",\"attachments\":[{\"color\":\"warning\",\"text\":\"$leaks\"}]}"\
+      https://hooks.slack.com/services/T027F3GAJ/B011TAG710V/${SLACK_AUTH_TOKEN}
 fi
